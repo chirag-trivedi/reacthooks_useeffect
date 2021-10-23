d0d7c9e5f4f3c549fe27a4c7e295243cefd85002
chirag-trivedi <chirag.trivedi@gmail.com>
2021-10-23 09:46:50 -0400

    react hooks useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+function DataFetching() {
+  const [albums, setalbums] = useState([])
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/albums')
+      .then(res => {
+        console.log(res)
+        setalbums(res.data)
+      }
+
+      )
+      .catch(err => {
+        console.log(err)
+      }
+
+      )
+  }
+
+  )
+
+  return(
+    <div>
+      <ul>
+        {albums.map(album => (
+          <li key={album.id}> {album.title} </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default DataFetching
